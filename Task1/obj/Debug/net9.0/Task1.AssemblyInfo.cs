b18d9f04a4703892b5803f7713fbe24356c7cce3
Apeksha-1-hub <meeraban123@gmail.com>
2025-11-26 16:46:07 +0545

    Task4 complete

--- Task1/obj/Debug/net9.0/Task1.AssemblyInfo.cs
+++ Task1/obj/Debug/net9.0/Task1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+499058bac1e848b043899b4742655756b049b796")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7dac06da0d306579f157238ded68b98eb4e0bce2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
